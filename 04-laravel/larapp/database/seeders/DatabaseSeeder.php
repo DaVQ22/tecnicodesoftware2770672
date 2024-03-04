c356ed109d018d6b79ee9beceeef9065c78f03ec
DaVQ22 <juandavidv644@gmail.com>
2024-03-04 17:00:27 -0500

    laravel changes

--- 04-laravel/larapp/database/seeders/DatabaseSeeder.php
+++ 04-laravel/larapp/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            UserSeeder::Class,
-            PetSeeder::Class
+            UserSeeder::class,
+            PetSeeder::class
 
         ]);
 
