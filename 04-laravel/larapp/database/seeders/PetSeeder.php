6999bda863325a43a06557eb934bb5334588751e
DaVQ22 <juandavidv644@gmail.com>
2024-03-04 16:54:31 -0500

    laravel changes

---
+++ 04-laravel/larapp/database/seeders/PetSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Pet;
+
+class PetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         // Add a record with Eloquent ORM
+         $pet = new pet;
+         $pet->name = 'Pocholo';
+         $pet->photo = "dog-working.jpg";
+         $pet->kind = "Dog";
+         $pet->weight = 10;
+         $pet->age = 4;
+         $pet->bread = "Pug";
+         $pet->location = "Manizales";
+         $pet->save();
+ 
+    }
+}
---
+++ 04-laravel/larapp/database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         // Add a record with Eloquent ORM
+         $user = new User;
+         $user->document = 75000001;
+         $user->fullname = "Jeremias Springfield";
+         $user->photo = "";
+         $user->phone = 310000001;
+         $user->email = "jeremias@gmail.com";
+         $user->password = bcrypt('admin');
+         $user->role = "admin";
+         $user->save();
+ 
+         // Add a record with Array 
+         DB::table('users')->insert([
+             'document' => 75000002,
+             'fullname' => 'John Wick',
+             'photo' => 'another-john-wick.jpg',
+             'phone' => 310000002,
+             'email' => 'johnw@gmail.com',
+             'password' => bcrypt('12345')
+ 
+         ]);
+    }
+}
